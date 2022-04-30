fd7e7f2633bf2bccf6d386a407e7cf1fe2b49ba1
Jaime Montes de Oca <jaime0422@live.com>
2022-04-29 20:14:17 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    if (leftNumbers > 0) {
+        const left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left - 3}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+
+    if (leftNumbers < 360) {
+        const left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left + 3}px`;
+    }
+}
+
+const dodger = document.getElementById("dodger");
+document.addEventListener("keydown", 
+    function (event) {
+        if (event.key === "ArrowLeft") {
+            moveDodgerLeft();
+        } else if (event.key === "ArrowRight") {
+            moveDodgerRight();
+        }
+    });
